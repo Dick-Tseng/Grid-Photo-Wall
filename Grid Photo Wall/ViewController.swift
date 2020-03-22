d6a7a87b8aedb06f812a30a37c9cbfad350ff278
Dick Tsemg <Author Name & Author Email>
2020-03-22 22:36:13 +0800

    Initial Commit

---
+++ Grid Photo Wall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grid Photo Wall
+//
+//  Created by Pingwei on 2020/3/22.
+//  Copyright © 2020 Dick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
